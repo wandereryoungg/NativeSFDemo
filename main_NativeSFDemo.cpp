1970de1adb8d2aa28495139254be59f70f69429a
yang.yang <yang.yang@vhd.com.cn>
2024-08-10 11:07:58 -0400

    SkCanvas drawSimpleText 不显示

--- main_NativeSFDemo.cpp
+++ main_NativeSFDemo.cpp
@@ -5,11 +5,19 @@
 #define LOG_TAG "NativeSFDemo"
 
 #include <binder/IPCThreadState.h>
-#include <binder/ProcessState.h>
 #include <binder/IServiceManager.h>
+#include <binder/ProcessState.h>
 #include <hardware/gralloc.h>
 #include <ui/GraphicBuffer.h>
 #include <utils/Log.h>
+#include <SkBitmap.h>
+#include <SkCanvas.h>
+#include <SkTypeface.h>
+#include <SkFontStyle.h>
+#include <SkFont.h>
+#include <SkStream.h>
+#include <SkEncodedImageFormat.h>
+#include <SkImageEncoder.h>
 
 #include "NativeSurfaceWrapper.h"
 
@@ -17,25 +25,32 @@ using namespace android;
 
 bool mQuit = false;
 
-void fillRGBA8Buffer(uint8_t* img, int width, int height, int stride, int r, int g, int b) {
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            uint8_t* pixel = img + (4 * (y*stride + x));
-            pixel[0] = r;
-            pixel[1] = g;
-            pixel[2] = b;
-            pixel[3] = 0;
-        }
+void saveBitmapToFile(const SkBitmap& bitmap, const char* filename) {
+    SkFILEWStream stream(filename);
+    if (!stream.isValid()) {
+        ALOGE("%s   Failed to create output file %s", __func__, filename);
+        return;
+    }
+
+    SkPixmap pixmap;
+    if (!bitmap.peekPixels(&pixmap)) {
+        ALOGE("%s   Failed to get bitmap pixels", __func__);
+        return;
+    }
+
+    bool ret = SkEncodeImage(&stream, pixmap, SkEncodedImageFormat::kPNG, 100);
+    if (!ret) {
+        ALOGE("%s   Failed to encode bitmap", __func__);
     }
 }
 
 int drawNativeSurface(sp<NativeSurfaceWrapper> nativeSurface) {
     status_t err = NO_ERROR;
     int countFrame = 0;
-    ANativeWindowBuffer *nativeBuffer = nullptr;
     ANativeWindow* nativeWindow = nativeSurface->getSurface().get();
 
-    // 1. connect the ANativeWindow as a CPU client. Buffers will be queued after being filled using the CPU
+    // 1. connect the ANativeWindow as a CPU client. Buffers will be queued
+    // after being filled using the CPU
     err = native_window_api_connect(nativeWindow, NATIVE_WINDOW_API_CPU);
     if (err != NO_ERROR) {
         ALOGE("ERROR: unable to native_window_api_connect\n");
@@ -43,14 +58,16 @@ int drawNativeSurface(sp<NativeSurfaceWrapper> nativeSurface) {
     }
 
     // 2. set the ANativeWindow dimensions
-    err = native_window_set_buffers_user_dimensions(nativeWindow, nativeSurface->width(), nativeSurface->height());
+    err = native_window_set_buffers_user_dimensions(
+        nativeWindow, nativeSurface->width(), nativeSurface->height());
     if (err != NO_ERROR) {
         ALOGE("ERROR: unable to native_window_set_buffers_user_dimensions\n");
         return EXIT_FAILURE;
     }
 
     // 3. set the ANativeWindow format
-    err = native_window_set_buffers_format(nativeWindow, PIXEL_FORMAT_RGBX_8888);
+    err =
+        native_window_set_buffers_format(nativeWindow, PIXEL_FORMAT_RGBX_8888);
     if (err != NO_ERROR) {
         ALOGE("ERROR: unable to native_window_set_buffers_format\n");
         return EXIT_FAILURE;
@@ -64,24 +81,31 @@ int drawNativeSurface(sp<NativeSurfaceWrapper> nativeSurface) {
     }
 
     // 5. set the ANativeWindow scale mode
-    err = native_window_set_scaling_mode(nativeWindow, NATIVE_WINDOW_SCALING_MODE_SCALE_TO_WINDOW);
+    err = native_window_set_scaling_mode(
+        nativeWindow, NATIVE_WINDOW_SCALING_MODE_SCALE_TO_WINDOW);
     if (err != NO_ERROR) {
-        ALOGE("native_window_set_scaling_mode failed: %s (%d)", strerror(-err), -err);
+        ALOGE("native_window_set_scaling_mode failed: %s (%d)", strerror(-err),
+              -err);
         return err;
     }
 
-    // 6. set the ANativeWindow permission to allocte new buffer, default is true
-    static_cast<Surface*>(nativeWindow)->getIGraphicBufferProducer()->allowAllocation(true);
+    // 6. set the ANativeWindow permission to allocte new buffer, default is
+    // true
+    static_cast<Surface*>(nativeWindow)
+        ->getIGraphicBufferProducer()
+        ->allowAllocation(true);
 
     // 7. set the ANativeWindow buffer count
     int numBufs = 0;
     int minUndequeuedBufs = 0;
 
-    err = nativeWindow->query(nativeWindow,
-            NATIVE_WINDOW_MIN_UNDEQUEUED_BUFFERS, &minUndequeuedBufs);
+    err = nativeWindow->query(
+        nativeWindow, NATIVE_WINDOW_MIN_UNDEQUEUED_BUFFERS, &minUndequeuedBufs);
     if (err != NO_ERROR) {
-        ALOGE("error: MIN_UNDEQUEUED_BUFFERS query "
-                "failed: %s (%d)", strerror(-err), -err);
+        ALOGE(
+            "error: MIN_UNDEQUEUED_BUFFERS query "
+            "failed: %s (%d)",
+            strerror(-err), -err);
         goto handle_error;
     }
 
@@ -93,66 +117,54 @@ int drawNativeSurface(sp<NativeSurfaceWrapper> nativeSurface) {
     }
 
     // 8. draw the ANativeWindow
-    while(!mQuit) {
-        // 9. dequeue a buffer
-        int releaseFenceFd = -1;
-        err = nativeWindow->dequeueBuffer(nativeWindow, &nativeBuffer, &releaseFenceFd);
-        if (err != NO_ERROR) {
-            ALOGE("error: dequeueBuffer failed: %s (%d)",
-                    strerror(-err), -err);
+    while (!mQuit) {
+        ANativeWindow_Buffer buffer;
+        if (ANativeWindow_lock(nativeWindow, &buffer, NULL) == 0) {
+
+            SkImageInfo info = SkImageInfo::Make(buffer.width, buffer.height, kRGBA_8888_SkColorType, kPremul_SkAlphaType);
+            ssize_t bytesPerLine = buffer.stride * bytesPerPixel(buffer.format);
+            ALOGI("%s   bytesPerLine: %zu", __func__, bytesPerLine);
+            SkBitmap bitmap;
+            bitmap.installPixels(info, buffer.bits, bytesPerLine);
+            // bitmap.allocPixels(info);
+
+            SkPaint paint;
+            paint.setAntiAlias(true);
+            paint.setColor(SK_ColorRED);
+
+            SkCanvas canvas(bitmap);
+
+            std::string tmp = "hello " + std::to_string(countFrame);
+            SkString drawStr(tmp);
+
+            SkFont font(SkTypeface::MakeFromName("monospace", SkFontStyle()), 20);
+
+            canvas.clear(SK_ColorTRANSPARENT);
+            canvas.drawColor(SK_ColorWHITE);
+
+            canvas.drawRect(SkRect::MakeLTRB(10, 10, 200, 200), paint);
+
+            canvas.drawSimpleText(drawStr.c_str(), drawStr.size(), SkTextEncoding::kUTF8, buffer.width / 2, buffer.height / 2, font, paint);
+            ALOGI("%s   draw Text: %s len: %zu", __func__, drawStr.c_str(), drawStr.size());
+
+            /*
+            static bool debug = true;
+            if (debug) {
+                saveBitmapToFile(bitmap, "/data/camera/young.png");
+                debug = false;
+            }
+            */
+
+            ANativeWindow_unlockAndPost(nativeWindow);
+            countFrame++;
+        } else {
+            ALOGE("%s   lock failed: %s", __func__, strerror(errno));
             break;
         }
-
-        // 10. make sure really control the dequeued buffer
-        sp<Fence> releaseFence(new Fence(releaseFenceFd));
-        int waitResult = releaseFence->waitForever("dequeueBuffer_EmptyNative");
-        if (waitResult != OK) {
-            ALOGE("dequeueBuffer_EmptyNative: Fence::wait returned an error: %d", waitResult);
-            break;
-        }
-
-        sp<GraphicBuffer> buf(GraphicBuffer::from(nativeBuffer));
-
-        // 11. Fill the buffer with black
-        uint8_t* img = nullptr;
-        err = buf->lock(GRALLOC_USAGE_SW_WRITE_OFTEN, (void**)(&img));
-        if (err != NO_ERROR) {
-            ALOGE("error: lock failed: %s (%d)", strerror(-err), -err);
-            break;
-        }
-
-        //12. Draw the window
-        countFrame = (countFrame+1)%3;
-        fillRGBA8Buffer(img, nativeSurface->width(), nativeSurface->height(), buf->getStride(),
-                        countFrame == 0 ? 255 : 0,
-                        countFrame == 1 ? 255 : 0,
-                        countFrame == 2 ? 255 : 0);
-
-        err = buf->unlock();
-        if (err != NO_ERROR) {
-            ALOGE("error: unlock failed: %s (%d)", strerror(-err), -err);
-            break;
-        }
-
-        // 13. queue the buffer to display
-        int acquireFenceFd = -1;
-        err = nativeWindow->queueBuffer(nativeWindow, buf->getNativeBuffer(), acquireFenceFd);
-        if (err != NO_ERROR) {
-            ALOGE("error: queueBuffer failed: %s (%d)", strerror(-err), -err);
-            break;
-        }
-
-        nativeBuffer = nullptr;
         sleep(1);
     }
 
 handle_error:
-    // 14. cancel buffer
-    if (nativeBuffer != nullptr) {
-        nativeWindow->cancelBuffer(nativeWindow, nativeBuffer, -1);
-        nativeBuffer = nullptr;
-    }
-
     // 15. Clean up after success or error.
     err = native_window_api_disconnect(nativeWindow, NATIVE_WINDOW_API_CPU);
     if (err != NO_ERROR) {
@@ -163,7 +175,7 @@ handle_error:
 }
 
 void sighandler(int num) {
-    if(num == SIGINT) {
+    if (num == SIGINT) {
         printf("\nSIGINT: Force to stop !\n");
         mQuit = true;
     }
@@ -172,7 +184,8 @@ void sighandler(int num) {
 int main() {
     signal(SIGINT, sighandler);
 
-    sp<NativeSurfaceWrapper> nativeSurface(new NativeSurfaceWrapper(String8("NativeSFDemo")));
+    sp<NativeSurfaceWrapper> nativeSurface(
+        new NativeSurfaceWrapper(String8("NativeSFDemo")));
     drawNativeSurface(nativeSurface);
     return 0;
 }
